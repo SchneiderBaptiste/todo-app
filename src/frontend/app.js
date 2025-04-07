f9ff9605c205cf9e2129a37371c52e6ceae38725
Schneider Baptiste <120581487+SchneiderBaptiste@users.noreply.github.com>
2025-04-07 14:50:44 +0200

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://agreeable-meadow-0f7035003.6.azurestaticapps.net/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
